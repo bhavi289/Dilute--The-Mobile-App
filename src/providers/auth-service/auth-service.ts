1f6122618212eaf530f5e360cf23fffdc4bf9042
bhavi289 <bhavi.c16@iiits.in>
2018-05-04 02:14:18 +0530

    ionic pro push

--- src/providers/auth-service/auth-service.ts
+++ src/providers/auth-service/auth-service.ts
@@ -4,7 +4,7 @@ import { Observable } from 'rxjs/Observable'
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 
-/*
+/* ionic pro
   Generated class for the AuthServiceProvider provider.
 
   See https://angular.io/docs/ts/latest/guide/dependency-injection.html
